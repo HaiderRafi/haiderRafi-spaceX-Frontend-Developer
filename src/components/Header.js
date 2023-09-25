5122bd2e0c64b350eb2d83e9aa1a063f310f8a6a
HaiderRafi <haiderrafi.7@gmail.com>
2023-09-25 23:39:02 +0530

    First Commit

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div className="">
+        <div>
+          <img
+            className="w-60"
+            src="https://www.metri-tech.com/wp-content/uploads/2017/01/spacex-logo.jpg"
+            alt="logo"
+          />
+        </div>
+        <h1>FALCON 9</h1>
+        <h1>FALCON HEAVY</h1>
+        <h1>DRAGON</h1>
+        <h1>STARSHIP</h1>
+        <h1>HUMAN SPACEFLIGHT</h1>
+        <h1>RIDESHARE</h1>
+        <h1>STARLINK</h1>
+      </div>
+    </>
+  );
+};
+
+export default Header;
